8de6b3ff0ee08c971c583a50d349a1db4b8638bc
Andreas Maunz <andreas@maunz.de>
2012-04-02 09:27:48 +0200

    Removed Java ENV

--- application.rb
+++ application.rb
@@ -1,14 +1,3 @@
-ENV["JAVA_HOME"] = "/usr/lib/jvm/java-6-sun" unless ENV["JAVA_HOME"]
-ENV["JOELIB2"] = File.join File.expand_path(File.dirname(__FILE__)),"java"
-deps = []
-deps << "#{ENV["JAVA_HOME"]}/lib/tools.jar"
-deps << "#{ENV["JAVA_HOME"]}/lib/classes.jar"
-deps << "#{ENV["JOELIB2"]}"
-jars = Dir[ENV["JOELIB2"]+"/*.jar"].collect {|f| File.expand_path(f) }
-deps = deps + jars
-ENV["CLASSPATH"] = deps.join(":")
-
-
 require 'rubygems'
 gem "opentox-ruby", "~> 3"
 require 'opentox-ruby'
