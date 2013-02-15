44067db50ab0edbd895368196376f2daa6c33aa2
Christoph Helma <helma@in-silico.ch>
2013-02-15 12:25:44 +0100

    sleep time in task error test reduced

--- webapp/test.rb
+++ webapp/test.rb
@@ -5,7 +5,7 @@ module OpenTox
     
     post '/dataset/test/error_in_task/?' do
       task = OpenTox::Task.create($task[:uri],@subjectid,{ RDF::DC.description => "error_in_task"}) do |task|
-        sleep 2
+        sleep 1
         internal_server_error "error_in_task_message"      
       end
       response['Content-Type'] = 'text/uri-list'
