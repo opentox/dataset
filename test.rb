06b1c089b23a31d1a0cf9576edc72fdf05dd0486
mguetlein <martin.guetlein@gmail.com>
2013-08-08 16:43:13 +0200

    add more test routines for error handling

--- test.rb
+++ test.rb
@@ -1,4 +1,4 @@
-#for testing the error handling
+#ATTENTION: for testing the error handling in test/task.rb, CODE_LINES are important
 
 module OpenTox
   class Application < Service
@@ -6,11 +6,18 @@ module OpenTox
     post '/dataset/test/error_in_task/?' do
       task = OpenTox::Task.run("error_in_task", @uri) do |task|
         sleep 1
-        internal_server_error "error_in_task_message"      
+        bad_request_error "bad_request_error_in_task"      
       end
       response['Content-Type'] = 'text/uri-list'
       halt 202,task.uri.to_s+"\n"
     end
-
+    
+    get '/dataset/test/plain_error/?' do
+      bad_request_error "plain_bad_request_error"      
+    end
+    
+    get '/dataset/test/plain_no_ot_error/?' do
+      nil.no_method_for_nil
+    end
   end
 end
